0f66c2847cb1082a85efe40af1165e0b2c59de8a
Aviscad <anthonyviscad@gmail.com>
2023-04-08 15:23:24 -0600

    Finishing up Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,31 @@
-import LogInSingIn from '../LogInSingIn/LogInSingIn'
+import imgUrl from '/src/assets/images/illustration-working.svg'
+
+import Button from '../Button/Button'
+import Navbar from '../Navbar/Navbar'
+import styles from './Header.module.scss'
+
 export const Header = () => {
 	return (
-		<div>
-			<LogInSingIn />
-		</div>
+		<header className={styles.container}>
+			<div className={styles.menu}>
+				<Navbar />
+			</div>
+			<div className={styles.content}>
+				<div>
+					<img
+						src={imgUrl}
+						alt='Woman working on a desktop pc'
+					/>
+				</div>
+				<div className={styles['header-text']}>
+					<h1 className='main-title'>More than just shorter links</h1>
+					<p className='paragraph-text'>
+						Build your brand’s recognition and get detailed insights on how your links are
+						performing.
+					</p>
+					<Button primary>Get Started</Button>
+				</div>
+			</div>
+		</header>
 	)
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,96 @@
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+
+import { ReactComponent as Logo } from '/src/assets/images/logo.svg'
+
+import LogSingIn from '../LogInSingIn/LogInSingIn'
+import styles from './Navbar.module.scss'
+
+const Navbar = () => {
+	const [open, setOpen] = useState(false)
+	const [mobileMenu, setMobileMenu] = useState(true)
+
+	useEffect(() => {
+		const windowResize = () => {
+			if (window.innerWidth > 700) {
+				setMobileMenu(false)
+				setOpen(true)
+			} else {
+				setMobileMenu(true)
+				setOpen(false)
+			}
+		}
+
+		if (window.innerWidth > 700) {
+			setMobileMenu(false)
+			setOpen(true)
+		}
+
+		window.addEventListener('resize', windowResize)
+
+		return () => {
+			window.removeEventListener('resize', windowResize)
+		}
+	}, [open])
+
+	const toggleChange = () => {
+		setOpen(!open)
+	}
+
+	return (
+		<nav className={styles.navbar}>
+			{mobileMenu && <Logo />}
+			<ul className={open ? styles.vissible : `${styles.vissible} hidden`}>
+				{!mobileMenu && (
+					<li className={styles['list-item']}>
+						<Logo />
+					</li>
+				)}
+				<li className={styles['list-item']}>
+					<a
+						href='https://github.com/Aviscad/url-shortener'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Features
+					</a>
+				</li>
+				<li className={styles['list-item']}>
+					<a
+						href='https://github.com/Aviscad/url-shortener'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Pricing
+					</a>
+				</li>
+				<li className={styles['list-item']}>
+					<a
+						href='https://github.com/Aviscad/url-shortener'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Resources
+					</a>
+				</li>
+				{mobileMenu && (
+					<li className={styles['list-item']}>
+						<LogSingIn />
+					</li>
+				)}
+			</ul>
+			{!mobileMenu && <LogSingIn />}
+			{mobileMenu && (
+				<FontAwesomeIcon
+					icon={!open ? faBars : faXmark}
+					onClick={toggleChange}
+					color='hsl(257, 7%, 63%)'
+					size='2x'
+					className={!open ? '' : styles['fade-in']}
+				/>
+			)}
+		</nav>
+	)
+}
+export default Navbar
