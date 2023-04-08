aaea30a94b52535650f8e5586afa8ff8af840fc7
Aviscad <anthonyviscad@gmail.com>
2023-04-08 12:49:46 -0600

    Folder structure and assets

---
+++ src/data/card-data.ts
@@ -0,0 +1,22 @@
+const CARDS_INFO = [
+	{
+		id: 11111,
+		title: 'Brand Recognition',
+		text: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+		icon: 'faChartLine',
+	},
+	{
+		id: 22222,
+		title: 'Detailed Records',
+		text: 'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+		icon: 'faGauge',
+	},
+	{
+		id: 33333,
+		title: 'Fully Customizable',
+		text: 'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+		icon: 'faPaintBrush',
+	},
+]
+
+export default CARDS_INFO
