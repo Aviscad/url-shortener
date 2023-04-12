e4293976ab23a93e2403f520e572e6473a41cb12
Aviscad <anthonyviscad@gmail.com>
2023-04-12 09:00:36 -0600

    Checking If Link Has Been Shortened

--- src/services/services.ts
+++ src/services/services.ts
@@ -9,3 +9,15 @@ export interface CardProps {
 	text: string
 	icon: string
 }
+
+export const isAlreadyShortened = (links: LinksType[], link: string) => {
+	const isShortened: LinksType[] = links.filter((l) => {
+		return l.original === link ||
+			l.original === 'http://' + link ||
+			l.original === 'https://' + link
+			? true
+			: false
+	})
+
+	return isShortened.length === 0 ? false : true
+}
